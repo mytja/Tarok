1136e1b17ffcb2d229a049359a16efcd9d7227b2
mytja <mytja@protonmail.com>
2023-08-13 09:34:08 +0200

    [backend] mešanje kart z vsaj enim tarokom - allegedly resolves #10

--- backend/internal/ws/server.go
+++ backend/internal/ws/server.go
@@ -162,6 +162,68 @@ func (s *serverImpl) Connect(w http.ResponseWriter, r *http.Request) Client {
 	return client
 }
 
+func (s *serverImpl) ShuffleCards(gameId string) {
+	for {
+		game, exists := s.games[gameId]
+		if !exists {
+			s.logger.Errorw("game has finished, it doesn't exist, exiting", "gameId", gameId)
+			return
+		}
+
+		cards := make([]consts.Card, 0)
+		cards = append(cards, consts.CARDS...)
+		rand.Shuffle(len(cards), func(i, j int) {
+			cards[i], cards[j] = cards[j], cards[i]
+		})
+		imaTaroka := false
+		for _, userId := range game.Starts {
+			imaTaroka = false
+			for i := 0; i < (54-6)/game.PlayersNeeded; i++ {
+				if strings.Contains(cards[0].File, "taroki") {
+					imaTaroka = true
+				}
+
+				game.Players[userId].BroadcastToClients(&messages.Message{
+					PlayerId: userId,
+					Data: &messages.Message_Card{
+						Card: &messages.Card{
+							Id:     cards[0].File,
+							UserId: userId,
+							Type: &messages.Card_Receive{
+								Receive: &messages.Receive{},
+							},
+						},
+					},
+				})
+				game.Players[userId].AddCard(Card{
+					id:     cards[0].File,
+					userId: userId,
+				})
+				cards = helpers.Remove(cards, 0)
+			}
+			if !imaTaroka {
+				break
+			}
+		}
+		if !imaTaroka {
+			s.logger.Errorw("igralec ni dobil taroka, ponovno mešam karte", "gameId", gameId)
+			continue
+		}
+
+		for i := 0; i < 6; i++ {
+			game.Talon = append(game.Talon, Card{
+				id: cards[0].File,
+			})
+			cards = helpers.Remove(cards, 0)
+		}
+		s.logger.Debugw("talon", "talon", game.Talon)
+
+		s.games[gameId] = game
+
+		return
+	}
+}
+
 func (s *serverImpl) StartGame(gameId string) {
 	s.logger.Debugw("game started. sending GameStart packet", "gameId", gameId)
 
@@ -224,41 +286,7 @@ func (s *serverImpl) StartGame(gameId string) {
 	// go je tok hiter da prehiteva dobesedno vse :)
 	time.Sleep(time.Millisecond * 20)
 
-	// Shuffle
-	cards := make([]consts.Card, 0)
-	cards = append(cards, consts.CARDS...)
-	rand.Shuffle(len(cards), func(i, j int) {
-		cards[i], cards[j] = cards[j], cards[i]
-	})
-	for _, userId := range game.Starts {
-		for i := 0; i < (54-6)/game.PlayersNeeded; i++ {
-			game.Players[userId].BroadcastToClients(&messages.Message{
-				PlayerId: userId,
-				Data: &messages.Message_Card{
-					Card: &messages.Card{
-						Id:     cards[0].File,
-						UserId: userId,
-						Type: &messages.Card_Receive{
-							Receive: &messages.Receive{},
-						},
-					},
-				},
-			})
-			game.Players[userId].AddCard(Card{
-				id:     cards[0].File,
-				userId: userId,
-			})
-			cards = helpers.Remove(cards, 0)
-		}
-	}
-
-	for i := 0; i < 6; i++ {
-		game.Talon = append(game.Talon, Card{
-			id: cards[0].File,
-		})
-		cards = helpers.Remove(cards, 0)
-	}
-	s.logger.Debugw("talon", "talon", game.Talon)
+	s.ShuffleCards(gameId)
 
 	// game must be reinitialized after s.ShuffleCards(...)
 	licitatesFirst := game.Players[game.Starts[0]]
@@ -403,10 +431,6 @@ func (s *serverImpl) Authenticated(client Client) {
 			}
 		}()
 	}
-}
-
-func (s *serverImpl) ShuffleCards(gameId string) {
-
 }
 
 func (s *serverImpl) KingCalling(gameId string) {
