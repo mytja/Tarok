d2f76e506ec271b11a8f347ec386a81d2513cc90
mytja <mytja@protonmail.com>
2023-08-16 21:58:16 +0200

    make cards smaller

--- tarok/assets/tarok/recursive.sh
+++ tarok/assets/tarok/recursive.sh
@@ -1,6 +1,6 @@
 find . -type f -name '*.webp' -exec sh -c '
     name="${1%.*}"
     echo "$name"
-    cwebp "$1" -jpeg_like -o "${name}.webp"
+    convert "$1" -resize 50% "${name}.webp"
 ' find-sh {} \;
 
