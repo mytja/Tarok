0a344924701ca13bbc4a56cc9cf2f023cf237282
mytja <mytja@protonmail.com>
2023-10-06 22:56:55 +0200

    [skip ci] remove deprecated Svelte frontend in favour of Flutter frontend too many security advisories on a deprecated project. this old frontend was highly performant, but was scrapped for more "native" and faster (offline) solutions (Flutter) rip old frontend :(

--- generateproto.sh
+++ generateproto.sh
@@ -1,3 +1,2 @@
-protoc --ts_out ./frontend/src --proto_path messages messages/*.proto
 protoc --dart_out ./tarok/lib --proto_path messages messages/*.proto
 protoc --proto_path=./messages --go_out=./ messages/*.proto
