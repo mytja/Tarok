809b04913b0b4211315aec638242b27b17527416
mytja <mytja@protonmail.com>
2023-11-01 15:26:54 +0100

    [skip ci] include copyright notices & license notices in every file

--- stockskis/lib/src/stockskis_base.dart
+++ stockskis/lib/src/stockskis_base.dart
@@ -1,3 +1,18 @@
+// StockŠkis - simple tarock bots.
+// Copyright (C) 2023 Mitja Ševerkar
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 // ignore_for_file: avoid_print, prefer_adjacent_string_concatenation, prefer_interpolation_to_compose_strings, library_prefixes
 
 import "dart:convert";
